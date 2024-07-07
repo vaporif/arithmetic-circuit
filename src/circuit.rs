e3d76fc9aba72cfd8d2149526c84818f61d23b07
Dmitriy Onypko <vaporif@gmail.com>
2024-07-07 22:26:13 +0100

    init

---
+++ src/circuit.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+pub enum Gate {
+    Input(String),
+    Add(Box<Node>, Box<Node>),
+    Mul(Box<Node>, Box<Node>),
+    Const(i32),
+}
+
+pub struct Node {
+    gate: Gate,
+    value: Option<i32>,
+}
+
+impl Node {
+    pub fn new(gate: Gate) -> Self {
+        Node { gate, value: None }
+    }
+
+    pub fn evaluate(&mut self, inputs: &HashMap<String, i32>) -> i32 {
+        if let Some(val) = self.value {
+            return val;
+        }
+
+        let result = match &mut self.gate {
+            Gate::Input(name) => *inputs.get(name).unwrap(),
+            Gate::Add(l, r) => l.evaluate(inputs) + r.evaluate(inputs),
+            Gate::Mul(l, r) => l.evaluate(inputs) * r.evaluate(inputs),
+            Gate::Const(val) => *val,
+        };
+
+        self.value = Some(result);
+        result
+    }
+}
+
+pub struct ArithmeticCircuit {}
